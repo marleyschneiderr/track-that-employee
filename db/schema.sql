b415e257c4b4914afa1a603c49846723096f693e
marleyschneiderr <marley33@uw.edu>
2023-03-26 18:12:24 -0700

    Adding new schema for schema/sql with dropping at the beginning for exisitng employee db

--- db/schema.sql
+++ db/schema.sql
@@ -1,25 +1,27 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
 CREATE TABLE department (
-  id INT NOT NULL PRIMARY KEY,
-  name VARCHAR(30) NOT NULL,
+    id INT AUTO_INCREMENT NOT NULL,
+    department_name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-  id INT NOT NULL PRIMARY KEY,
-  title VARCHAR(30) NOT NULL,
-  salary DECIMAL NOT NULL,
-  department_id INT,
-  FOREIGN KEY (department_id)
-  REFERENCES department(id)
-  ON DELETE SET NULL
+    id INT AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10, 0) NOT NULL,
+    department_id INT,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE employee (
-  id INT NOT NULL PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  role_id INT,
-  manager_id INT REFERENCES employee(id),
-  FOREIGN KEY (role_id)
-  REFERENCES role(id)
-  ON DELETE SET NULL,
+    id INT AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    PRIMARY KEY (id)
 );

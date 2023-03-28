db51bcff2e1a544c17065b4376755c8da0fa838b
marleyschneiderr <marley33@uw.edu>
2023-03-27 20:53:08 -0700

    Trying to fix syntax, missed comma and i function was incorrectly defined before

--- index.js
+++ index.js
@@ -269,45 +269,51 @@ const newDepartment = async () => {
 
 // time to add a new role into the db
 const insertRole = async () => {
-    try {
-        console.log('Add Role');
+  try {
+    console.log("Add Role");
 
-        let departments = await connection.query("Select * From department")
+    let departments = await connection.query("Select * From department");
 
-        let answer = await inquirer.prompt([
-            {
-                name: 'title',
-                type: 'input',
-                message: 'Please type the name of your new role.'
-            },
-            {
-                name: 'salary',
-                type: 'input',
-                message: 'Please insert related salary to this role.'
-            },
-            {
-                name: 'idDepartment',
-                type: 'list',
-                choices: departments.map((idDepartment) => {
-                    return {
-                        name: idDepartment.department_name,
-                        value: idDepartment.id
-                    }
-                }),
-                message: 'List the department ID this role is associated with.',
-            }
-        ]);
+    let answer = await inquirer.prompt([
+      {
+        name: "title",
+        type: "input",
+        message: "Please type the name of your new role.",
+      },
+      {
+        name: "salary",
+        type: "input",
+        message: "Please insert related salary to this role.",
+      },
+      {
+        name: "idDepartment",
+        type: "list",
+        choices: departments.map((idDepartment) => {
+          return {
+            name: idDepartment.department_name,
+            value: idDepartment.id,
+          };
+        }),
+        message: "List the department ID this role is associated with.",
+      },
+    ]);
 
-            let selectedDepartment;
-            for (i = 0; i < departments.length; i++) {
-                if(departments[i].department_id === answer.choice) {
-                    selectedDepartment = departments[i];
-                };
-            }
-            let result = await connection.query("Insert INTO role SET ?, {
-                title: answer.title,
-                salary: answer.salary
-            })
-        
+    let selectedDepartment;
+    for (i = 0; i < departments.length; i++) {
+      if (departments[i].id === answer.idDepartment) {
+        selectedDepartment = departments[i];
+      }
     }
-}
+    let result = await connection.query("INSERT INTO role SET ?", {
+      title: answer.title,
+      salary: answer.salary,
+      department_id: answer.idDepartment,
+    });
+
+    console.log(`${answer.title} role inserted successfully.\n`);
+    runProgram();
+  } catch (err) {
+    console.log(err);
+    runProgram();
+  }
+};
